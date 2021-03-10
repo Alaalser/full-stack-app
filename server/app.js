078a959dacd6c9644ed3855e088ea8df41cdab39
Alaa Alser <alaalsser@gmail.com>
2021-03-11 01:21:03 +0200

    setup the server

--- server/app.js
+++ server/app.js
@@ -1,3 +1,21 @@
-const express = require(express)
+require('dotenv').config();
 
-express()
+const express = require('express');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+
+const router = require('./router')
+
+const app = express();
+
+app.set('port',process.env.PORT || 5000);
+app.disable('x-powered-by');
+
+app.use(express.json());
+app.use(compression());
+app.use(compression());
+app.use(express.urlencoded({ extended: false}));
+
+app.use('/api/v1', router);
+
+module.exports = app;
