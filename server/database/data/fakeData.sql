cab4d8b75475130cfdfe27c5cf6bc6d4b8e26ef1
Alaa Alser <alaalsser@gmail.com>
2021-03-12 00:55:07 +0200

    edit scheema and fake data

--- server/database/data/fakeData.sql
+++ server/database/data/fakeData.sql
@@ -1,14 +1,14 @@
 /* ADD USER */
 
-INSERT INTO users(user,email,password)
+INSERT INTO users(user_name,email,password)
 VALUES ('alaa','alaa@alaa.com','alaa@alaa.com');
-INSERT INTO users(user,email,password)
+INSERT INTO users(user_name,email,password)
 VALUES ('moh','moh@moh.com','moh@moh.com');
-INSERT INTO users(user,email,password) 
+INSERT INTO users(user_name,email,password) 
 VALUES ('messi','messi@messi.com','$messi@messi.com');
 
 /* ADD NEWS */
-INSERT INTO news(id,title,description,image) VALUES(1,'fake title','faje description','https://images.unsplash.com/photo-1584714268709-c3dd9c92b378?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dXJsJTIwbmV3c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
-INSERT INTO news(id,title,description,image) VALUES(2,'fake title','faje description','https://images.unsplash.com/photo-1584714268709-c3dd9c92b378?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dXJsJTIwbmV3c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
-INSERT INTO news(id,title,description,image) VALUES(3,'fake title','faje description','https://images.unsplash.com/photo-1584714268709-c3dd9c92b378?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dXJsJTIwbmV3c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
-INSERT INTO news(id,title,description,image) VALUES(4,'fake title','faje description','https://images.unsplash.com/photo-1584714268709-c3dd9c92b378?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dXJsJTIwbmV3c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+INSERT INTO news(id,title,description,image) VALUES (1,'fake title','faje description','https://images.unsplash.com/photo-1584714268709-c3dd9c92b378?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dXJsJTIwbmV3c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60');
+INSERT INTO news(id,title,description,image) VALUES (2,'fake title','faje description','https://images.unsplash.com/photo-1584714268709-c3dd9c92b378?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dXJsJTIwbmV3c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60');
+INSERT INTO news(id,title,description,image) VALUES (3,'fake title','faje description','https://images.unsplash.com/photo-1584714268709-c3dd9c92b378?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dXJsJTIwbmV3c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60');
+INSERT INTO news(id,title,description,image) VALUES (4,'fake title','faje description','https://images.unsplash.com/photo-1584714268709-c3dd9c92b378?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8dXJsJTIwbmV3c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
