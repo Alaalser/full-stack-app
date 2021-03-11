65f69620b419294fb0803643d659a5a8eca65fdb
Alaa Alser <alaalsser@gmail.com>
2021-03-11 02:22:44 +0200

    edit bulid.js

--- server/database/data/bulid.js
+++ server/database/data/bulid.js
@@ -5,7 +5,7 @@ const connection = require('./connection');
 const dbBuild = () => {
   let sql = readFileSync(join(__dirname, 'build.sql')).toString();
   if (process.env.NODE_ENV === 'test') {
-    sql += readFileSync(join(__dirname, 'fake.sql')).toString();
+    sql += readFileSync(join(__dirname, 'fakeData.sql')).toString();
   }
   return connection.query(sql);
 };
