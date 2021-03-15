a4e0cdd7de8a8fdd7b83d75f1307bd76616753d5
Alaa Alser <alaalsser@gmail.com>
2021-03-15 23:30:14 +0200

    add simple style

--- client/src/Pages/Signup/index.js
+++ client/src/Pages/Signup/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Button from '../../Components/Button';
 import Input from '../../Components/Input';
+import './style.css';
 
 const Signup = () => (
-  <form>
+  <form className="form">
     <Input type="text" id="name" placeholder="user name" />
     <Input type="email" id="email" placeholder="email@expamle.com" />
     <Input type="password" id="password" placeholder="enter your password" />
