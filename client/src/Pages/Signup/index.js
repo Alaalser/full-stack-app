d03ee3c0d0b56e71863f1e0c449f8253aab13aa9
Alaa Alser <alaalsser@gmail.com>
2021-03-15 23:19:25 +0200

    created sign up form componet

---
+++ client/src/Pages/Signup/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from '../../Components/Button';
+import Input from '../../Components/Input';
+
+const Signup = () => (
+  <form>
+    <Input type="text" id="name" placeholder="user name" />
+    <Input type="email" id="email" placeholder="email@expamle.com" />
+    <Input type="password" id="password" placeholder="enter your password" />
+    <Input
+      type="password"
+      id="confirmedPassword"
+      placeholder="password again"
+    />
+    <Button text="Sign up" />
+  </form>
+);
+
+export default Signup;
