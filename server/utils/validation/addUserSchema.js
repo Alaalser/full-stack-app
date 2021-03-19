8e7ff09f69815b0ea1041277a0c56cd8e4975246
Alaa Alser <alaalsser@gmail.com>
2021-03-19 22:09:57 +0200

    add messages for signup & login schema

--- server/utils/validation/addUserSchema.js
+++ server/utils/validation/addUserSchema.js
@@ -1,12 +1,16 @@
 const { string, object, ref } = require('yup');
 
 const addUserSchema = object({
-  userName: string().min(4).required(),
-  email: string().email().required(),
-  password: string().min(8).required(),
+  userName: string()
+    .min(4, 'User name must be at least 4 char')
+    .required('User Name is required'),
+  email: string().email().required('email Name is required'),
+  password: string()
+    .min(8, 'Password must be at least 8 char')
+    .required('Password is required'),
   confirmPassword: string()
-    .oneOf([ref('password'), null])
-    .required(),
+    .oneOf([ref('password'), null], 'Passwords are not match')
+    .required('confirmPassword is required'),
 });
 
 module.exports = addUserSchema;
--- server/utils/validation/loginSchema.js
+++ server/utils/validation/loginSchema.js
@@ -2,7 +2,7 @@ const { object, string } = require('yup');
 
 const loginSchema = object({
   email: string().email().required(),
-  password: string().required().min(8),
+  password: string().required().min(8, 'Password must be at least 8 char'),
 });
 
 module.exports = loginSchema;
