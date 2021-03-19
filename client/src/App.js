5bbfc4e78e5691ae7c2dd767b6d8cfd8422b3d39
Alaa Alser <alaalsser@gmail.com>
2021-03-19 03:30:32 +0200

    initalize home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Pages/Home';
 import Signup from './Pages/Signup';
+import Login from './Pages/Login';
 
 const App = () => (
-  <div className="App">
-    <Signup />
-  </div>
+  <Router>
+    <Switch>
+      <div className="App">
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </div>
+    </Switch>
+  </Router>
 );
 
 export default App;
